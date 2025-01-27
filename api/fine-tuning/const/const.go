41a2ff36a3317f0f5a66a8d88e9c40edeb3d0aec
0g-peterzhb <158457852+0g-peterzhb@users.noreply.github.com>
2025-01-27 13:54:13 +0800

    Feat(fine-tuning): Add deepseek 1.5B model (#112)

--- api/fine-tuning/const/const.go
+++ api/fine-tuning/const/const.go
@@ -12,5 +12,6 @@ var (
 	SCRIPT_MAP = map[string]string{
 		"0x8645816c17a8a70ebf32bcc7e621c659e8d0150b1a6bfca27f48f83010c6d12e": "/app/finetune-img.py",
 		"0x7f2244b25cd2219dfd9d14c052982ecce409356e0f08e839b79796e270d110a7": "/app/finetune.py",
+		"0x2084fdd904c9a3317dde98147d4e7778a40e076b5b0eb469f7a8f27ae5b13e7f": "/app/finetune.py",
 	}
 )
